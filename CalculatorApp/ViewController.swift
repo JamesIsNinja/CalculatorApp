eeb67ca5f2f45cb4321c8606a0ef08788422e3b5
student <student@students-mac.student.georgianc.on.ca>
2016-02-03 15:06:08 -0500

    The Layout works? What wizardry is this?

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var CalcLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func OneClicked(sender: AnyObject) {
+        CalcLabel.text = sender.currentTitle!!
+    }
 
 }
 
